f2425a5a219dd43abb83a5c6e6d360e0ef857713
Theo <theo.morel195@gmail.com>
2026-02-17 16:45:51 +0100

    - Add integration tests to verify /joint_states and controller status - Add smoke tests for launch files and configuration resources - Fix all linter issues (flake8, pep257, xmllink) for PEP8 compliance

--- arm_mine_explorer/arm_mine_explorer_gazebo/launch/gazebo.launch.py
+++ arm_mine_explorer/arm_mine_explorer_gazebo/launch/gazebo.launch.py
@@ -1,100 +1,114 @@
-#!/usr/bin/env python3
+# Copyright 2026 Theo Morel
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, RegisterEventHandler, IncludeLaunchDescription
-from launch.conditions import IfCondition, UnlessCondition
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import (
-    Command,
-    FindExecutable,
-    LaunchConfiguration,
-    PathJoinSubstitution,
-)
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-
-    # -----------------------
+    """Launch the simulation of the robotic arm in gazebo."""
     # Packages
-    # -----------------------
     arm_description_pkg_share = FindPackageShare('arm_mine_explorer_description')
     arm_gazebo_pkg_share = FindPackageShare('arm_mine_explorer_gazebo')
 
-    # -----------------------
     # Arguments
-    # -----------------------
     declared_arguments = [
-        DeclareLaunchArgument("prefix", default_value=''),
-        DeclareLaunchArgument("name", default_value='MobileBaseMineExplorerSystem'),
-        DeclareLaunchArgument("use_sim", default_value='true'),
-        
-        DeclareLaunchArgument("joint_limit_params", 
-            default_value=PathJoinSubstitution([arm_description_pkg_share, 'config', 'joint_limits.yaml'])),
-        DeclareLaunchArgument("kinematics_params", 
-            default_value=PathJoinSubstitution([arm_description_pkg_share, 'config', 'default_kinematics.yaml'])),
-        DeclareLaunchArgument("physical_params", 
-            default_value=PathJoinSubstitution([arm_description_pkg_share, 'config', 'physical_parameters.yaml'])),
-        DeclareLaunchArgument("visual_params", 
-            default_value=PathJoinSubstitution([arm_description_pkg_share, 'config', 'visual_parameters.yaml'])),
-        DeclareLaunchArgument("initial_positions_file", 
-            default_value=PathJoinSubstitution([arm_description_pkg_share, 'config', 'initial_positions.yaml'])),
-        
-        DeclareLaunchArgument("safety_limits", default_value="true"),
-        DeclareLaunchArgument("safety_pos_margin", default_value="0.15"),
-        DeclareLaunchArgument("safety_k_position", default_value="20"),
-        DeclareLaunchArgument("world", 
-            default_value=PathJoinSubstitution([arm_gazebo_pkg_share, 'worlds', 'empty_world.model']))
+        DeclareLaunchArgument('prefix', default_value=''),
+        DeclareLaunchArgument('name', default_value='ArmMineExplorerSystem'),
+        DeclareLaunchArgument('use_sim', default_value='true'),
+        DeclareLaunchArgument(
+            'joint_limit_params',
+            default_value=PathJoinSubstitution([
+                arm_description_pkg_share, 'config', 'joint_limits.yaml'
+            ])),
+        DeclareLaunchArgument(
+            'kinematics_params',
+            default_value=PathJoinSubstitution([
+                arm_description_pkg_share, 'config', 'default_kinematics.yaml'
+            ])),
+        DeclareLaunchArgument(
+            'physical_params',
+            default_value=PathJoinSubstitution([
+                arm_description_pkg_share, 'config', 'physical_parameters.yaml'
+            ])),
+        DeclareLaunchArgument(
+            'visual_params',
+            default_value=PathJoinSubstitution([
+                arm_description_pkg_share, 'config', 'visual_parameters.yaml'
+            ])),
+        DeclareLaunchArgument(
+            'initial_positions_file',
+            default_value=PathJoinSubstitution([
+                arm_description_pkg_share, 'config', 'initial_positions.yaml'
+            ])),
+        DeclareLaunchArgument('safety_limits', default_value='true'),
+        DeclareLaunchArgument('safety_pos_margin', default_value='0.15'),
+        DeclareLaunchArgument('safety_k_position', default_value='20'),
+        DeclareLaunchArgument(
+            'world',
+            default_value=PathJoinSubstitution([
+                arm_gazebo_pkg_share, 'worlds', 'empty_world.model'
+            ]))
     ]
 
-    # -----------------------
     # Robot description
-    # -----------------------
     robot_description_file = Command([
-        PathJoinSubstitution([FindExecutable(name='xacro')]), ' ',
-        PathJoinSubstitution([arm_description_pkg_share, 'urdf', 'robot.urdf.xacro']), ' ',
-        'prefix:=', LaunchConfiguration("prefix"), ' ',
-        'name:=', LaunchConfiguration("name"), ' ',
-        'use_sim:=', LaunchConfiguration("use_sim"), ' ',
-        'joint_limit_params:=', LaunchConfiguration("joint_limit_params"), ' ',
-        'kinematics_params:=', LaunchConfiguration("kinematics_params"), ' ',
-        'physical_params:=', LaunchConfiguration("physical_params"), ' ',
-        'visual_params:=', LaunchConfiguration("visual_params"), ' ',
-        'initial_positions_file:=', LaunchConfiguration("initial_positions_file"), ' ',
-        'safety_limits:=', LaunchConfiguration("safety_limits"), ' ',
-        'safety_pos_margin:=', LaunchConfiguration("safety_pos_margin"), ' ',
-        'safety_k_position:=', LaunchConfiguration("safety_k_position"),
+        PathJoinSubstitution([
+            FindExecutable(name='xacro')]),
+        ' ',
+        PathJoinSubstitution([
+            arm_description_pkg_share, 'urdf', 'robot.urdf.xacro'
+        ]),
+        ' ',
+        'prefix:=', LaunchConfiguration('prefix'),
+        ' ',
+        'name:=', LaunchConfiguration('name'),
+        ' ',
+        'use_sim:=', LaunchConfiguration('use_sim'),
+        ' ',
+        'joint_limit_params:=', LaunchConfiguration('joint_limit_params'),
+        ' ',
+        'kinematics_params:=', LaunchConfiguration('kinematics_params'),
+        ' ',
+        'physical_params:=', LaunchConfiguration('physical_params'),
+        ' ',
+        'visual_params:=', LaunchConfiguration('visual_params'),
+        ' ',
+        'initial_positions_file:=', LaunchConfiguration('initial_positions_file'),
+        ' ',
+        'safety_limits:=', LaunchConfiguration('safety_limits'),
+        ' ',
+        'safety_pos_margin:=', LaunchConfiguration('safety_pos_margin'),
+        ' ',
+        'safety_k_position:=', LaunchConfiguration('safety_k_position'),
     ])
 
-    # -----------------------
-    # ros2_control
-    # -----------------------
-    controller_manager_config = PathJoinSubstitution(
-        [
-            arm_gazebo_pkg_share
-            'config',
-            'gazebo_controller_manager.yaml',
-        ]
-    )
-
-    # -----------------------
     # Robot state publisher
-    # -----------------------
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[
-            {'robot_description': robot_description},
-        ],
+        parameters=[{'robot_description': robot_description_file}],
         output='screen',
     )
 
-    # -----------------------
     # Controllers
-    # -----------------------
     joint_state_broadcaster_spawner = Node(
         package='controller_manager',
         executable='spawner',
@@ -122,9 +136,7 @@ def generate_launch_description():
         )
     )
 
-    # -----------------------
     # Gazebo
-    # -----------------------
     gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             PathJoinSubstitution(
@@ -137,7 +149,7 @@ def generate_launch_description():
         ),
         launch_arguments={
             'verbose': 'false',
-            'world': LaunchConfiguration("world"),
+            'world': LaunchConfiguration('world'),
         }.items(),
     )
 
@@ -151,9 +163,7 @@ def generate_launch_description():
         output='screen',
     )
 
-    # -----------------------
     # Launch
-    # -----------------------
     return LaunchDescription(
         declared_arguments + [
             gazebo_launch,
